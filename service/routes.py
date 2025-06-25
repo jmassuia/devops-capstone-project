5ad8c1ac0e0b061215d3eb0fbaa36211ac9946cf
Joao Massuia <massuia1507@gmail.com>
2025-06-24 20:56:11 -0400

    adding read and test cases

--- service/routes.py
+++ service/routes.py
@@ -67,9 +67,18 @@ def create_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
+@app.route("/accounts/<int:account_id>",methods=["GET"])
+def get_accounts(account_id):
+    app.logger.info("Request to read an Account with id %s",account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND,f"Account with id [{account_id} could not be found.]")
+    return account.serialize(),status.HTTP_200_OK
 # ... place you code here to READ an account ...
-
+def test_get_account_not_found(self):
+    """It should not Read an Account that is not found"""
+    resp = self.client.get(f"{BASE_URL}/0")
+    self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
